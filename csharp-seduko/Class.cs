a4d2a1f7f8e6a3e24e2f8526e425fb5550348745
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-02 09:43:49 -0600

    Adding Switch case to direct game

---
+++ csharp-seduko/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		static void Output()
+        {
+			string name = "Hello from class";
+			Console.WriteLine(name);
+        }
+
+		static void Main()
+        {
+			Output();
+		}
+
+	}
+}
--- csharp-seduko/Program.cs
+++ csharp-seduko/Program.cs
@@ -4,6 +4,28 @@ namespace csharp_seduko
 {
     class Program
     {
-        
+        static string ShowOptions()
+        {
+            Console.WriteLine("Options: \nShow Options again - S");
+            Console.WriteLine("End Game - E");
+            string UserInput = Console.ReadLine();
+            return UserInput;
+        }
+        static void Main()
+        {
+            string UserInput = ShowOptions();
+
+
+           switch (UserInput)
+            {
+                case "S":
+                    Console.WriteLine("Show Instrutsion again");
+                    ShowOptions();
+                    break;
+                case "E":
+                    Console.WriteLine("End Game");
+                    break;
+            }
+        }
     }
 }
