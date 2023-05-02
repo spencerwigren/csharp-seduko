08feae89a525805dd7134ad5770cfb707bde8dd9
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-02 08:00:19 -0600

    Test File Creation

---
+++ TestFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+        double NewNumber = 1.345;
+
+        static void AddNumbers(double Number1)
+        {
+            int Number2 = 2;
+
+            double Number3 = Number1 + Number2;
+
+            Console.WriteLine(Number3);
+        }
+
+        static void NewMethod(string fname)
+        {
+            Console.WriteLine(fname);
+        }
+        static void Main()
+        {
+            NewMethod("Test Name");
+            Console.WriteLine("Hello World!\nthis is a test");
+        }
+
+    }
+}
