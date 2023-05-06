5251495ea92e506e2a577de26c7cbef4512ce1d8
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-06 08:29:15 -0600

    Method Displaying the board

--- csharp-seduko/Program.cs
+++ csharp-seduko/Program.cs
@@ -5,11 +5,12 @@ namespace csharp_seduko
 {
     class Program
     {
+
         static void ReadFile()
         {
-            // method to read a file
-            // could not get "SolutionsFile.txt" to load in current directory
-            // will come back to find a solutions
+            /* method to read a file
+            could not get "SolutionsFile.txt" to load in current directory
+            will come back to find a solutions */
    
             // reads txt file
             if (File.Exists("SolutionsFile.txt"))
@@ -25,6 +26,57 @@ namespace csharp_seduko
             }
         }
 
+        static void DisplayBoard(int[,] Board)
+        {
+            /*
+             Will display the board, when player calls for it
+             */
+            
+            int count = 1;
+            int SideNumber = 2;
+            string BoardSectionDivisor = "  -+-+-+-+-+-+-+-+-";
+
+            Console.Write("  A B C D E F G H I\n1 ");
+
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    // Display number from int multi array
+                    if (Board[i, j] == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(Board[i, j]);
+                    }
+
+                    // Drawing the look of the board
+                    if (count % 9 == 0)
+                    {
+                        Console.WriteLine();
+                        if (SideNumber < 10)
+                        {
+                            Console.WriteLine(BoardSectionDivisor);
+                            Console.Write(SideNumber++ + " ");
+                        }
+                        count++;
+                        
+                    }
+                    else
+                    {
+                        Console.Write("|");
+                        count++;
+                    }
+
+                }
+                
+            }
+            Console.WriteLine();
+        }
+
         static string UserInput()
         {
             // get user input
@@ -36,19 +88,25 @@ namespace csharp_seduko
         static void ShowOptions()
         {
             Console.WriteLine("Options: \nShow Options again - S");
+            Console.WriteLine("Display Baord - D");
             Console.WriteLine("Load File - F");
             Console.WriteLine("End Game - E\n");
         }
 
-        static void ShowRunner(bool RunGame)
+        static void ShowRunner(bool RunGame, int[,] Board)
         {
-            // this is control options for playing the game
+            /* This Method will control options for playing the game s*/
             while (RunGame == true)
             {
                 switch (UserInput().ToUpper())
                 {
                     case "S":
                         Console.WriteLine("Show Instrutsion again");
+                        ShowOptions();
+                        break;
+                    case "D":
+                        Console.WriteLine("Displing Board");
+                        DisplayBoard(Board);
                         ShowOptions();
                         break;
                     case "F":
@@ -65,10 +123,22 @@ namespace csharp_seduko
 
         static void Main()
         {
+
+            int[,] Board = {
+                {5,3,0,0,7,0,0,0,0},
+                {6,0,0,1,9,5,0,0,0},
+                {0,9,8,0,0,0,0,6,0},
+                {8,0,0,0,6,0,0,0,3},
+                {4,0,0,8,0,3,0,0,1},
+                {7,0,0,0,2,0,0,0,6},
+                {0,6,0,0,0,0,2,8,0},
+                {0,0,0,4,1,9,0,0,5},
+                {0,0,0,0,8,0,0,7,9}
+                };
             bool RunGame = true;
 
             ShowOptions();
-            ShowRunner(RunGame);
+            ShowRunner(RunGame, Board);
 
         }
     }
